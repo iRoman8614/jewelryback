993ad63426a1ffa2510a63b9e9a670c5587bff0d
Roman Baranovskii <rom8614@gmail.com>
2025-07-11 18:57:48 +0300

    cart check api 4

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,6 +1,7 @@
-import DeliveryOption from '../models/DeliveryOption.js';
 import PaymentMethod from '../models/PaymentMethod.js';
+import Category from '../models/Category.js';
 import Product from '../models/Product.js';
+import DeliveryOption from '../models/DeliveryOption.js';
 import { Op } from 'sequelize';
 
 export const getAllCheckoutOptions = async (req, res, next) => {
